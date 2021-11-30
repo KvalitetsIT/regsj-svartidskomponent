eeaf81f429de9274d428a2343b140a3398c1b364
Jonas Pedersen <jpe@kvalitetsit.dk>
2021-11-30 16:17:18 +0100

    Fix docker image name during build.

--- build/failOnFirstBuild.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-echo "${GITHUB_REPOSITORY}"
-echo "${DOCKER_SERVICE}"
-if [ "${GITHUB_REPOSITORY}" != "KvalitetsIT/regsj-svartidskomponent" ] && [ "${DOCKER_SERVICE}" = "kvalitetsit/regsj-svartidskomponent" ]; then
-  echo "Please run setup.sh REPOSITORY_NAME"
-  exit 1
-fi
--- documentation/docker/getOldDoc.sh
+++ documentation/docker/getOldDoc.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if docker pull kvalitetsit/regsj-svartidskomponent-documentation:latest; then
+if docker pull regsj/regsj-svartidskomponent-documentation:latest; then
     echo "Copy from old documentation image."
-    docker cp $(docker create kvalitetsit/regsj-svartidskomponent-documentation:latest):/usr/share/nginx/html target/old
+    docker cp $(docker create regsj/regsj-svartidskomponent-documentation:latest):/usr/share/nginx/html target/old
 fi
